62f7b758086b4b1d9601fbbd1ef2807affb1c7c4
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-10-25 16:30:23 -0300

    feat: makeSUT

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,24 @@
+import { HttpPostClient } from 'data/protocols/http'
 import { HttpPostClientSpy } from '../../test'
 import { RemoteAuthentication } from './remote-authentication'
+
+type SutTypes = {
+  sut: RemoteAuthentication,
+  httpPostClientSpy: HttpPostClient
+}
+
+const makeSut = (url: string): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut, httpPostClientSpy
+  }
+}
 
 describe('RemoteAuthentcation', () => {
   test('Should call HttpPostClient with correct URL', () => {
     const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    const { sut, httpPostClientSpy } = makeSut(url)
     sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
