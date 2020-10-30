dc89e6f889147c709ba3f2714e3a1a898810a3ae
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-10-30 01:11:08 -0300

    Implementa Authentication e responses

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,17 +1,27 @@
 import { HttpPostClient } from 'data/protocols/http'
-import { AuthenticationParams } from 'domain/usecases'
+import { HttpStatusCode } from 'data/protocols/http/http-responses'
+import { InvalidCredentialsError } from 'domain/errors/invalid-credentials-error'
+import { AccountModel } from 'domain/models/account-model'
+import { Authentication, AuthenticationParams } from 'domain/usecases'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient
   ) {}
 
-  async auth(params: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: return Promise.resolve({
+        accessToken: ''
+      })
+    }
   }
 }
 
