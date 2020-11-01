bb55b47ab590984efcc74108110e8f49c2551412
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-01 01:55:01 -0300

    refactor: tipagens genéricas

--- src/data/test/mock-http-post-client.ts
+++ src/data/test/mock-http-post-client.ts
@@ -1,16 +1,16 @@
 import { HttpPostClient, HttpPostParams } from 'data/protocols/http'
 import { HttpResponse, HttpStatusCode } from 'data/protocols/http/http-responses'
 
-export class HttpPostClientSpy implements HttpPostClient {
+export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
 
-  body?: object
+  body?: T
 
-  response: HttpResponse = {
+  response: HttpResponse<R> = {
     statusCode: HttpStatusCode.ok
   }
 
-  async post(params: HttpPostParams): Promise<HttpResponse> {
+  async post(params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url
     this.body = params.body
     return Promise.resolve(this.response)
