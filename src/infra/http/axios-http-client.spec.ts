e1e4c0a0513e06a37ca392aa57ff471e51f6232e
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-02 15:34:52 -0300

    cria makeSut

--- src/infra/http/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client.spec.ts
@@ -5,9 +5,11 @@ import { AxiosHttpClient } from './axios-http-client'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const makeSut = (): AxiosHttpClient => new AxiosHttpClient()
+
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct URL', async () => {
-    const sut = new AxiosHttpClient()
+    const sut = makeSut()
     const url = faker.internet.url()
     sut.post({
       url
