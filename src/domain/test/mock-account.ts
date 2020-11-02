f84191d2c5b9095b994933ea7715158788abab91
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-02 13:58:05 -0300

    mock Account renamed

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,7 +1,12 @@
+import { AccountModel } from 'domain/models/account-model'
 import faker, { fake } from 'faker'
 import { AuthenticationParams } from '../usecases'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.random.uuid()
 })
