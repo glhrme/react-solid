9e489ae0aa8f5538cbcebeea23f65249ca83315e
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-13 21:27:00 -0300

    refatora spinner

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { HTMLAttributes } from 'react'
 import Styles from './spinner-styles.scss'
 
-const Spinner: React.FC = () => (
-  <div className={Styles.spinner}>
-    <div />
-    <div />
-    <div />
-  </div>
-)
+type Props = HTMLAttributes<HTMLElement>
+
+const Spinner: React.FC<Props> = (props: Props) => {
+  const { className } = props
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <div {...props} className={[Styles.spinner, className].join(' ')}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  )
+}
 
 export default Spinner
