50b321cf5d64d0ecf7bdde17c07a1a1c2612a256
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-13 21:12:20 -0300

    cria spinner

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './spinner-styles.scss'
+
+const Spinner: React.FC = () => (
+  <div className={Styles.spinner}>
+    <div />
+    <div />
+    <div />
+  </div>
+)
+
+export default Spinner
