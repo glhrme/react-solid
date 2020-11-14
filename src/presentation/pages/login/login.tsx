2c9a21a69eeac42bda5768a3570d831c6ed72ddd
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-13 21:27:32 -0300

    finaliza layout de login

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,35 @@
 import React from 'react'
+import Spinner from 'presentation/components/spinner'
 import Styles from './login-styles.scss'
+import { LOGO64 } from './contants'
 
-const Login: React.FC = () => <h1 className={Styles.login}>Oi Login</h1>
+const Login: React.FC = () => (
+  <div className={Styles.login}>
+    <header className={Styles.header}>
+      <img src={LOGO64} alt="Logo 4Devs" />
+      <h1>4Devs - Enquetes para Programadores</h1>
+    </header>
+    <form className={Styles.form}>
+      <h2>Login</h2>
+      <div className={Styles.inputWrap}>
+        <input type="email" name="email" placeholder="Digite seu e-mail" />
+        <span className={Styles.status}>🔴</span>
+      </div>
+      <div className={Styles.inputWrap}>
+        <input type="password" name="password" placeholder="Digite sua senha" />
+        <span className={Styles.status}>🔴</span>
+      </div>
+      <button className={Styles.btnSubmit} type="submit">Entrar</button>
+      <span className={Styles.link}>Criar conta</span>
+      <div className={Styles.errorWrap}>
+        <Spinner className={Styles.spinner} />
+        <span className={Styles.error}>
+          Error
+        </span>
 
+      </div>
+    </form>
+    <div className={Styles.footer} />
+  </div>
+)
 export default Login
