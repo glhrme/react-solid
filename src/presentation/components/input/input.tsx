da40f1c5792d00dccdf78920c570a8ff7d3a7946
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-01-13 19:29:42 -0300

    desativa autocomplete do chrome

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -3,10 +3,16 @@ import Styles from './input-styles.scss'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-const Input: React.FC<Props> = (props: Props) => (
-  <div className={Styles.inputWrap}>
-    <input {...props} />
-    <span className={Styles.status}>🔴</span>
-  </div>
-)
+const Input: React.FC<Props> = (props: Props) => {
+  const enableInput = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.readOnly = false
+  }
+
+  return (
+    <div className={Styles.inputWrap}>
+      <input {...props} readOnly onFocus={enableInput} />
+      <span className={Styles.status}>🔴</span>
+    </div>
+  )
+}
 export default Input
